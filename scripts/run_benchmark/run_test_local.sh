b82da72e1f30b4ac8e5d445f6743780ce9383e88
Luke Zappia <luke@data-intuitive.com>
2024-12-17 14:14:25 +0000

    Exclude UCE from local benchmark test

--- scripts/run_benchmark/run_test_local.sh
+++ scripts/run_benchmark/run_test_local.sh
@@ -21,6 +21,7 @@ input_states: s3://openproblems-data/resources_test/task_label_projection/**/sta
 rename_keys: 'input_train:output_train;input_test:output_test;input_solution:output_solution'
 output_state: "state.yaml"
 publish_dir: "$publish_dir"
+settings: '{"methods_exclude": ["uce"]}'
 HERE
 
 nextflow run . \
